b92e351707151b83b690b54caa905e1751751b4a
mounirA2m <mounir.ammam@outlook.com>
2024-03-17 12:11:43 -0400

    i added the sum and substract function with a main function that calls em

--- index.py
+++ index.py
@@ -1,2 +1,14 @@
 print("hello there")
 print("i added this line of code")
+
+def sum(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def main():
+    print(sum(5, 10))
+    print(subtract(10, 5))
+
+main()
